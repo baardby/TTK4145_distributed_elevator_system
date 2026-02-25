7dd70a928b86665bb347f97930655d634b2d5dd7
MagnusFlaa <magnfl@stud.ntnu.no>
2026-02-25 11:34:36 +0100

    Skrevet litt til timerne i Supervisor

--- supervisor/supervisor.go
+++ supervisor/supervisor.go
@@ -1,5 +1,28 @@
 package supervisor //Må endres hvis det puttes inn i en mappe
 
+import (
+	"time"
+)
+
+type elevatorTimer struct {
+	startTime time.Time
+	duration  time.Duration
+}
+
+type supervisor struct {
+	elevatorTimerList []elevatorTimer
+}
+
+func (s *supervisor) resetElevatorTimer(elevatorID int) {
+	s.elevatorTimerList[elevatorID].startTime = time.Now()
+}
+
+func (s *supervisor) checkElevatorTimer(elevatorID int) bool {
+	currentTime := time.Now()
+	elapsedTime := currentTime.Sub(s.elevatorTimerList[elevatorID].startTime)
+	return elapsedTime > s.elevatorTimerList[elevatorID].duration
+}
+
 func Supervisor() {
 	processPairs()
 }
@@ -11,27 +34,6 @@ func Supervisor() {
 //func listenForErrors
 //		input: error
 //		kjør restart, eller løs problemet.
-//		return: none
-
-//func passOnInfo
-//		input: message from network_listener
-//		Gir meldingen mening? 					//Kan kanskje klare oss uten
-//		Del opp info
-//		kaller elevatorInfo og request_queueInfo
-//		Resetter timer på elevator #
-//		return: none
-
-//func request_queueInfo
-//		input: køen til en annen heis
-//		kaller queueUnion
-//		return: none
-
-//func elevatorInfo
-//		input: info om elevator #
-//		sjekker om infoen er ulik den gamle
-//		hvis ulik:								//sjekker ikke om stemmer, siden ny info mottas hele tiden
-//			kaller updateElevatorStates med input elevator #
-//		else: none
 //		return: none
 
 //func watchDogTimer							//Hva er denne egt til?
