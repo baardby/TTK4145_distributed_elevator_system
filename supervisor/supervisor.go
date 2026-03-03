827c33108c4a654cc38bdb905a053fdc7324c2f5
MagnusFlaa <magnfl@stud.ntnu.no>
2026-03-03 12:12:02 +0100

    Removed time from Supervisor, changed the structure of main loop

--- supervisor/supervisor.go
+++ supervisor/supervisor.go
@@ -1,39 +1,59 @@
 package supervisor //Må endres hvis det puttes inn i en mappe
 
 import (
-	"time"
+	"distributed_elevator/HealthTimers"
 )
 
-type elevatorTimer struct {
-	startTime time.Time
-	duration  time.Duration
+type Ports struct {
+	TimerCommand chan<- HealthTimers.TimerCommand
+	TimerEvent   <-chan HealthTimers.TimerEvent
+
+	//Legg til flere kanaler for de forskjellige modulene
 }
 
-type supervisor struct {
-	elevatorTimerList []elevatorTimer
+func handleTimeEvent(timeEvent int) {
+	//handle time event
 }
 
-func (s *supervisor) resetElevatorTimer(elevatorID int) {
-	s.elevatorTimerList[elevatorID].startTime = time.Now()
+func handleHardwareEvent(hardwareEvent int) {
+	//handle hardware event
 }
 
-func (s *supervisor) checkElevatorTimer(elevatorID int) bool {
-	currentTime := time.Now()
-	elapsedTime := currentTime.Sub(s.elevatorTimerList[elevatorID].startTime)
-	return elapsedTime > s.elevatorTimerList[elevatorID].duration
+func handleNetwork_ListenerEvent(network_listenerEvent int) {
+	//handle network listener event
 }
 
 func Supervisor() {
-	processPairs()
+	backupPhase()
+	//listen for others queue
+	//if no response, init queue
+	//make all the channels and init funktion for the different modules
+	//set own state
+	timeChan := make(chan int)             //channel for timers
+	hardwareChan := make(chan int)         //channel for hardware events
+	network_listenerChan := make(chan int) //channel for network listener events
+
+	for {
+		//send checkpoint to backup
+		select {
+		case timeEvent := <-timeChan:
+			handleTimeEvent(timeEvent)
+		case hardwareEvent := <-hardwareChan:
+			handleHardwareEvent(hardwareEvent)
+		case network_listenerEvent := <-network_listenerChan:
+			handleNetwork_ListenerEvent(network_listenerEvent)
+			//default: //kjør acceptance test, og send melding til backup
+		}
+	}
 }
 
 //func amIWorking
 //		input: none
 //		forskjellgie tester på meg selv
+//			Kjører jeg når jeg skal kjøre?
+//		return: bool
 
 //func listenForErrors
 //		input: error
 //		kjør restart, eller løs problemet.
 //		return: none
-
-//func watchDogTimer							//Hva er denne egt til?
